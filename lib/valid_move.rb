2b9597b336b75c77bb4b07af97e43d01c2c6229a
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-16 16:49:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,12 +12,8 @@ end
 
 
 def valid_move?(board,index)
-  if (index <= 8)
+  if (index <= 8) && (position_taken? == false)
     true
-  elsif (index <= 8) && (position_taken? == false)
-    true
-  elsif position_taken? == true
-    false
   end
 end
 
