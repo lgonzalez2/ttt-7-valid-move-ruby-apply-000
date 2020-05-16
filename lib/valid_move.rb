e0fb8ce41712bcb0fce41efe9ff2d3f31b5eb93f
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-16 16:48:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
-
+def position_taken?(board,index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
+  end
+end
 
 
 def valid_move?(board,index)
